368afd6d58fe05e09be090f34ff1fbc0d24c89d5
wooyoung6685 <wooyoung6685@gmail.com>
2023-03-21 15:18:04 +0900

    결재완료

--- models/product.js
+++ models/product.js
@@ -1,25 +1,30 @@
-module.exports=(sequelize,DataTypes)=>{
-  const product=sequelize.define("Product",{
-    name:{
-      type:DataTypes.STRING(100),
-      allowNull:false,
+module.exports = (sequelize, DataTypes) => {
+  const product = sequelize.define("Product", {
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
     },
-    price:{
-      type:DataTypes.INTEGER(10),
-      allowNull:false,
+    price: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false,
     },
-    seller:{
-      type:DataTypes.STRING(30),
-      allowNull:false,
+    seller: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
     },
-    description:{
-      type:DataTypes.STRING(300),
-      allowNull:false,
+    description: {
+      type: DataTypes.STRING(300),
+      allowNull: false,
     },
-    imageUrl:{
-      type:DataTypes.STRING(300),
-      allowNull:true,
+    imageUrl: {
+      type: DataTypes.STRING(300),
+      allowNull: true,
     },
-  })
+    soldout: {
+      type: DataTypes.INTEGER(1),
+      allowNull: false,
+      defaultValue: 0,
+    },
+  });
   return product;
-}
+};
